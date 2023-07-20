a7dd2740cec6da4adb5a00752a4d45d4151802bb
Angel Gao <angelgao9@gmail.com>
2023-07-20 17:50:18 -0400

    completed 01

--- projects/from-javascript-to-typescript/the-typeinator/03-callbacks-to-async-await/index.js
+++ projects/from-javascript-to-typescript/the-typeinator/03-callbacks-to-async-await/index.js
@@ -1,5 +1,26 @@
 // Put your checkEmotion and speak functions here! ✨
 // See ./original.js for their older JavaScript code.
 
+const checkEmotion = async (knownEmotions, emotion, callback) {
+     setTimeout(() => {
+        await callback(knownEmotions.has(emotion))
+    }, 1000)
+
+}
+
+const speak = async (knownEmotions, newEmotion, phrase, callback) => {
+    await checkEmotion(knownEmotions, newEmotion, (hasEmotion) => {
+        if (hasEmotion) {
+            callback(`"${phrase}" (${newEmotion})`)
+        }
+        else {
+            callback (
+                undefined,
+                new Error(`Does not compute. I do not understand ${newEmotion}.`)
+            )
+        }
+    })
+}
+
 module.exports.checkEmotion = checkEmotion;
 module.exports.speak = speak;
